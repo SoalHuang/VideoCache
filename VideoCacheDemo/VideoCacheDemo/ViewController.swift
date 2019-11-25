975a19a6850ace78a52039bc591ac2506c586f1a
soso <huangziliang@putao.com>
2019-11-25 15:31:13 +0800

    demo upgrade to swift5

--- VideoCacheDemo/VideoCacheDemo/ViewController.swift
+++ VideoCacheDemo/VideoCacheDemo/ViewController.swift
@@ -16,6 +16,7 @@ private let VideoCacheVersionKey = "VideoCacheVersionKey"
 class ViewController: UIViewController {
     
     func setupVideoCache() {
+        
         VideoCacheManager.default.logLevel = .request
         VideoCacheManager.default.capacityLimit = Int64(1).GB
         
@@ -37,12 +38,9 @@ class ViewController: UIViewController {
         addChild(playerViewController)
         view.addSubview(playerViewController.view)
         
-//        let url = URL(string: "https://sylvan.apple.com/Videos/comp_A006_C003_1219EE_CC_v01_SDR_PS_FINAL_20180709_SDR_2K_AVC.mov")!
-//        let url = URL(string: "https://sylvan.apple.com/Videos/KP_A010_C002_SDR_20190717_SDR_2K_AVC.mov")!
-//        let url = URL(string: "https://vod.putaocdn.com/postman.mp4?auth_key=1574514306-6275-0-752f6c637ce71ff00b0840461203fa44")!
-        let url = URL(string: "https://vod.putaocdn.com/KP_A010_C002_SDR_20190717_SDR_2K_AVC.mov?auth_key=1574661737-3194-0-25102037fbc6c7d4c6fe3a0f980dd56e")!
+        let url = URL(string: "<#Video or Audio URL#>")!
+        let cacheItem = AVPlayerItem(remote: url, cacheKey: <#Store Key or nil#>)
         
-        let cacheItem = AVPlayerItem(remote: url)
         playerViewController.player = AVPlayer(playerItem: cacheItem)
         playerViewController.player?.play()
     }
