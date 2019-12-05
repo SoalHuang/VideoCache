93859dc4ec187fe1c8ea366352c80dbe990d5fd3
soso <huangziliang@putao.com>
2019-12-05 14:15:31 +0800

    update AVPlayerItem Extension

--- VideoCache/AVPlayerItem+Cache.swift
+++ VideoCache/AVPlayerItem+Cache.swift
@@ -15,15 +15,14 @@ extension AVPlayerItem {
     public convenience init(manager: VideoCacheManager = VideoCacheManager.default,
                             remote url: URL,
                             cacheKey key: String? = nil,
-                            cacheRanges: [VideoRange] = []) {
+                            cacheRanges: [VideoRange] = [VideoRange(0, VideoRangeBounds.max)]) {
         
         let `key` = key ?? url.absoluteString.videoCacheMD5
         
         let videoUrl = VURL(cacheKey: key, originUrl: url)
         manager.use(url: videoUrl)
         
-        let ranges = cacheRanges.count > 0 ? cacheRanges : [VideoRange(0, VideoRangeBounds.max)]
-        let loaderDelegate = VideoResourceLoaderDelegate(manager: manager, url: videoUrl, cacheRanges: ranges)
+        let loaderDelegate = VideoResourceLoaderDelegate(manager: manager, url: videoUrl, cacheRanges: cacheRanges)
         let urlAsset = AVURLAsset(url: loaderDelegate.url.includeVideoCacheSchemeUrl, options: nil)
         urlAsset.resourceLoader.setDelegate(loaderDelegate, queue: .main)
         
